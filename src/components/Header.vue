371fe11145f222fdeb9fd62fa6daf8f5d9229490
HawkingHuang <math0915@gmail.com>
2025-04-02 15:45:03 +0800

    header to input

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,11 +7,35 @@
     <div class="bg-[#9e1d1d] h-[37px]">
       <div class="container h-full flex justify-between items-center">
         <div class="flex text-primary-text text-sm">
-          <button class="px-2">比比昂日本購物</button>
-          <div class="px-2">綜合服務</div>
+          <button class="px-2 cursor-pointer hover:text-primary-text-hover">比比昂日本購物</button>
+          <div class="px-2 cursor-pointer hover:text-primary-text-hover">綜合服務</div>
         </div>
-        <div></div>
+        <div class="flex text-primary-text text-sm">
+          <button class="px-2 cursor-pointer hover:text-primary-text-hover">最近看過</button>
+          <button class="px-2 cursor-pointer hover:text-primary-text-hover">購物車</button>
+          <button class="px-2 cursor-pointer hover:text-primary-text-hover">我的訂單</button>
+          <button class="px-2 cursor-pointer hover:text-primary-text-hover">顧客中心</button>
+          <button class="px-2 cursor-pointer hover:text-primary-text-hover">下載 APP</button>
+          <button class="cursor-pointer hover:text-primary-text-hover">登出</button>
+        </div>
       </div>
+    </div>
+
+    <div class="container flex pt-4 pb-2">
+      <img src="/images/c2aeb346-6b77-4a0b-bc96-f41e3d1be76d.gif" alt="" class="w-[142px] h-[60px]">
+      <div class="px-7.5">
+        <form class="flex items-center bg-[#fff] rounded-lg w-[540px] h-[45px]">
+          <div>全站</div>
+          <div></div>
+          <div>
+            <button class="bg-primary text-primary-text rounded-lg">搜尋</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <div>
+      
     </div>
   </div>
 </template>
