ad791437ef0fc7f75fc2fc402c49a83900b74802
HawkingHuang <math0915@gmail.com>
2025-04-02 12:39:18 +0800

    two colors config

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,12 @@
 </script>
 
 <template>
-  <div class="bg-[#ea1717] h-[172px]">
+  <div class="bg-primary h-[172px]">
     <div class="bg-[#9e1d1d] h-[37px]">
       <div class="container h-full flex justify-between items-center">
-        <div class="flex">
-          <button>比比昂日本購物</button>
-          <div>綜合服務</div>
+        <div class="flex text-primary-text text-sm">
+          <button class="px-2">比比昂日本購物</button>
+          <div class="px-2">綜合服務</div>
         </div>
         <div></div>
       </div>
