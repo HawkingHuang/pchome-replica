e36915f54e9b5f21d68062d74f5b7432172dcd96
HawkingHuang <math0915@gmail.com>
2025-04-02 12:18:14 +0800

    header background

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-[#ea1717] h-[172px]">
+    <div class="bg-[#9e1d1d] h-[37px]">
+      <div class="container h-full flex justify-between items-center">
+        <div class="flex">
+          <button>比比昂日本購物</button>
+          <div>綜合服務</div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
